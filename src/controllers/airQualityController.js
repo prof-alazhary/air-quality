87236ef9a5669544b4344ff0ecca737205c7951f
Mohamed ElAzhary <mohamed.elazhary@vodafone.com>
2024-03-22 22:59:09 +0200

    feat: call the IQAIR API to get the real info.

--- src/controllers/airQualityController.js
+++ src/controllers/airQualityController.js
@@ -1,9 +1,15 @@
+const axios = require('axios');
+const API_KEY = '6b441523-dd55-4aad-91b9-9e4594a5dc9a';
+
 module.exports = {
     getAirQuality: async (req, res) => {
-        const { longitude, latitude } = req.params;
-        console.log(longitude, latitude);
         try {
-            res.json({ results: "air quality info..." });
+            const { longitude, latitude } = req.params;
+            console.log(longitude, latitude)
+
+            const response = await axios.get(`http://api.airvisual.com/v2/nearest_city?lat=${longitude}&lon=${longitude}&key=${API_KEY}`);
+            const airQualityInfo = response.data?.data?.current;
+            res.json({ results: airQualityInfo });
         } catch (error) {
             console.error('Error fetching air quality:', error);
             res.status(500).json({ error: 'Internal server error' });
