97cf094fe3e163799126a1cb1234d7906edf50c9
Mohamed ElAzhary <mohamed.elazhary@vodafone.com>
2024-03-25 00:37:28 +0200

    refactor: Env variables.
    - adding and setup the dotenv package.
    - creating the .env file to hold the env vars.
    - replacing the hardcoded values by the env vars.

--- src/controllers/airQualityController.js
+++ src/controllers/airQualityController.js
@@ -1,13 +1,14 @@
 const axios = require('axios');
-const API_KEY = '6b441523-dd55-4aad-91b9-9e4594a5dc9a';
+const API_KEY = process.env.API_KEY;
+const IQAIR_API_URL = process.env.IQAIR_API_URL;
+const NEAREST_CITY_API = process.env.NEAREST_CITY_API;
 
 module.exports = {
     getAirQuality: async (req, res) => {
         try {
             const { longitude, latitude } = req.params;
             console.log(longitude, latitude)
-
-            const response = await axios.get(`http://api.airvisual.com/v2/nearest_city?lat=${latitude}&lon=${longitude}&key=${API_KEY}`);
+            const response = await axios.get(`${IQAIR_API_URL}${NEAREST_CITY_API}?lat=${latitude}&lon=${longitude}&key=${API_KEY}`);
             const airQualityInfo = response.data?.data?.current;
             res.json({ results: airQualityInfo });
         } catch (error) {
