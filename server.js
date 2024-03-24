bbe0c7ee7ff428303d54a89009cd1b15efe0c8ca
Mohamed ElAzhary <mohamed.elazhary@vodafone.com>
2024-03-25 00:45:35 +0200

    refactor: adding NODE_SERVER env vars.

--- server.js
+++ server.js
@@ -5,11 +5,12 @@ const airQualityRoutes = require('./src/routes/airQualityRoutes');
 
 const app = express();
 const port = process.env.NODE_SERVER_PORT || 3000;
+const serverAddress = process.env.NODE_SERVER_ADDRESS || 'http://localhost';
 
 app.use(express.json());
 
 app.use('/api/air-quality', airQualityRoutes);
 
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+    console.log(`Server is running on ${serverAddress}:${port}`);
 });
